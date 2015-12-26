51539170b28345404139cebc7d11f5703b9ecf0d
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-12-26 10:46:56 +0100

    Perfect FenwickTree

--- FenwickTree.java
+++ FenwickTree.java
@@ -1,4 +1,7 @@
 
+/**
+* All indexes in FenwickTree are 1-based.
+*/
 public class FenwickTree {
 	private final int[] add;
 	private final int[] mul;
@@ -10,32 +13,65 @@ public class FenwickTree {
 		this.n = n;
 	}
 
-	public void pointUpdate(int index, int value) {
+	public FenwickTree(int[] A) {
+		this(A.length);
+		for (int i = 0; i < A.length; i++) {
+			this.addValue(i + 1, A[i]);
+		}
+	}	
+
+	private void internalUpdate(final int index, int factor, int value) {
 		assert index > 0 && index <= n;
-		while (index <= n) {
-			add[index] += value;
-			index += (index & -index);
+		for (int i = index; i <= n; i += (i & -i)) {
+			mul[i] += factor;
+			add[i] += value;
 		}
 	}
 
-	public int pointQuery(int index) {
+	// @return sum(A[1]..A[index])
+	public int getSum(final int index) {
 		assert index >= 0 && index <= n;
 		int sum = 0;
-		while (index > 0) {
-			sum += add[index];
-			index -= (index & -index);
+		int fact = 0;
+		for (int i = index; i > 0; i -= (i & -i)) {
+			sum += add[i];
+			fact += mul[i];
 		}
-		return sum;		
+		return fact * index + sum;		
 	}
 
-	public int rangeQuery(int low, int high) {
+	// @return sum(A[low]..A[high])
+	public int getRangeSum(int low, int high) {
 		assert low <= high;
-		return pointQuery(high) - pointQuery(low - 1);
+		return getSum(high) - getSum(low - 1);
 	}
 
-	public void rangeUpdate(int low, int high, int value) {
-		for (int i = low; i <= high; i++) {
-			this.pointUpdate(i, value);
-		}
+	// @return A[index]
+	public int getValue(int index) {
+		assert index > 0 && index <= n;
+		return getSum(index) - getSum(index - 1);
+	}	
+
+	// A[index] += value
+	public void addValue(int index, int value) {
+		internalUpdate(index, 0, value);
+	}	
+
+	// for (int i = low; i <= high; i++) A[i] += value
+	public void addValueToRange(int low, int high, int value) {
+		internalUpdate(low, value, -value * (low - 1));
+		internalUpdate(high, -value, value * high); 
 	}
+
+	// @return A[]
+	public int[] toArray() {
+		int[] A = new int[n];
+		int cumul = 0;
+		for (int i = 0; i < A.length; i++) {
+			A[i] = this.getSum(i + 1) - cumul;
+			cumul += A[i];
+		}
+		return A;
+	}
+
 }
--- FenwickTreeTest.java
+++ FenwickTreeTest.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class FenwickTreeTest {
+
 	private Random random = new Random();
 
 	private int[] randomArray(int n) {
@@ -13,15 +14,6 @@ public class FenwickTreeTest {
 		return A;
 	}
 
-	private int[] cumulArray(int[] A) {
-		int[] C = new int[A.length];
-		for (int i = 0, cumul = 0; i < A.length; i++) {
-			cumul += A[i];
-			C[i] = cumul;
-		}
-		return C;
-	}
-
 	private int rangeSum(int[] A, int low, int high) {
 		int sum = 0;
 		for (int i = low; i <= high; i++) {
@@ -30,57 +22,81 @@ public class FenwickTreeTest {
 		return sum;
 	}
 
-	private void rangeUpdate(int[] A, int low, int high, int value) {
+	private void addValueToRange(int[] A, int low, int high, int value) {
 		for (int i = low; i <= high; i++) {
 			A[i] += value;
 		}
 	}	
 
 	@Test
-	public void testPointUpdate() {
+	public void testToArray() { 
 		int n = 2000;
 		int[] A = randomArray(n);
-		int[] C = cumulArray(A);
-		FenwickTree fw = new FenwickTree(n);
-		for (int i = 0; i < A.length; i++) {
-			fw.pointUpdate(i + 1, A[i]);
-		}
-		for (int i = 0; i < A.length; i++) {
-			assertEquals(C[i], fw.pointQuery(i + 1));
-		}
-		for (int i = 0; i < 3 * n; i++) {
-			int a = random.nextInt(n);
-			int b = random.nextInt(n);
-			if (a > b) {
-				int t = a;
-				a = b;
-				b = t;
-			}
-			assertEquals(rangeSum(A, a, b), fw.rangeQuery(a + 1, b + 1));	
-		}
-	}
+		FenwickTree fw = new FenwickTree(A);		
+		int[] B = fw.toArray();
+		assertArrayEquals(A, B);
+	}	
 
 	@Test
-	public void testRangeUpdate() {
+	public void testAddValue() { 
+		int n = 2000;
+		FenwickTree fw = new FenwickTree(n);		
+		int[] A = randomArray(n);
+		for (int i = 0; i < A.length; i++) {
+			fw.addValue(i + 1, A[i]);
+		}
+		int[] B = fw.toArray();
+		assertArrayEquals(A, B);
+	}	
+
+	@Test
+	public void testGetValue() { 
 		int n = 2000;
 		int[] A = randomArray(n);
-		FenwickTree fw = new FenwickTree(n);
+		FenwickTree fw = new FenwickTree(A);		
 		for (int i = 0; i < A.length; i++) {
-			fw.pointUpdate(i + 1, A[i]);
+			assertEquals(A[i], fw.getValue(i + 1));
 		}
-		for (int i = 0; i < 3 * n; i++) {
-			int a = random.nextInt(n);
-			int b = random.nextInt(n);
-			if (a > b) {
-				int t = a;
-				a = b;
-				b = t;
+	}		
+
+	@Test
+	public void testGetSum() { 
+		int n = 2000;
+		int[] A = randomArray(n);
+		FenwickTree fw = new FenwickTree(A);		
+		for (int i = 0, cumul = 0; i < A.length; i++) {
+			cumul += A[i];
+			assertEquals(cumul, fw.getSum(i + 1));
+		}
+	}		
+
+	@Test
+	public void testGetRangeSum() { 
+		int n = 500;
+		int[] A = randomArray(n);
+		FenwickTree fw = new FenwickTree(A);		
+		for (int i = 0; i < A.length; i++) {
+			for (int j = i; j < A.length; j++) {
+				int sum = rangeSum(A, i, j);
+				assertEquals(sum, fw.getRangeSum(i + 1, j + 1));
 			}
-			assertEquals(rangeSum(A, a, b), fw.rangeQuery(a + 1, b + 1));	
-			int val = random.nextInt(1000);
-			rangeUpdate(A, a, b, val);
-			fw.rangeUpdate(a + 1, b + 1, val);
 		}
+	}		
+
+	@Test
+	public void testAddValueToRange() {
+		int n = 500;
+		int[] A = randomArray(n);
+		FenwickTree fw = new FenwickTree(A);		
+		for (int i = 0; i < A.length; i++) {
+			for (int j = i; j < A.length; j++) {
+				int value = random.nextInt(100);
+				addValueToRange(A, i, j, value);
+				fw.addValueToRange(i + 1, j + 1, value);
+			}
+		}
+		int[] B = fw.toArray();
+		assertArrayEquals(A, B);
 	}	
 
 }
